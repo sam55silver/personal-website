db4f3aa6ed8d84f37baf64cd677c1b78fdbfb022
Sam Silver <sam@Sams-MacBook-Air.local>
2022-03-18 14:17:11 -0300

    Added the intro to the site

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Sam Silver</h1>
+        <div className="button-row">
+          <button>Projects</button>
+          <button>Resume</button>
+          <button>Blog</button>
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default App
