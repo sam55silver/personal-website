91deaeaa700a92b44a39f33883036bfd4622e56c
Sam Silver <sam55silver@gmail.com>
2022-05-04 17:32:10 -0300

    Added links to social media and started adding routes for website

--- src/components/Nav/Navigation.jsx
+++ src/components/Nav/Navigation.jsx
@@ -6,22 +6,43 @@ import {
   faGithub,
   faLinkedin,
 } from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router-dom";
 
 function Navigation() {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <div className="nav-bar">
-      <h2>SS.</h2>
+      <h2 onClick={scrollToTop}>SS.</h2>
       <div className="button-row">
-        <button>Blog</button>
-        <button>Projects</button>
-        <button>Resume</button>
-        <button>About</button>
+        <Link to="/blog">
+          <button>Blog</button>
+        </Link>
+        <Link to="/projects">
+          <button>Projects</button>
+        </Link>
+        <Link to="/resume">
+          <button>Resume</button>
+        </Link>
+        <Link to="/about">
+          <button>About</button>
+        </Link>
       </div>
       <div className="social-media">
-        <FontAwesomeIcon icon={faGithub} />
-        <FontAwesomeIcon icon={faLinkedin} />
-        <FontAwesomeIcon icon={faTwitter} />
-        <FontAwesomeIcon icon={faInstagram} />
+        <a href="https://github.com/sam55silver">
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a href="https://www.linkedin.com/in/sam-silver-375851239/">
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+        <a href="https://twitter.com/sam55silver">
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+        <a href="https://www.instagram.com/sam_silver06/">
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
       </div>
     </div>
   );
