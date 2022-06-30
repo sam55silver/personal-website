29c01f2ca48b85a290d538e5e3b91badf955d9f6
Sam Silver <sam55silver@gmail.com>
2022-06-29 21:38:48 -0300

    Added slightly more style to posts

--- components/Navigation.js
+++ components/Navigation.js
@@ -3,14 +3,14 @@ import { MenuAlt3Icon } from "@heroicons/react/solid";
 
 function Navigation() {
   return (
-    <section className="sticky top-0 bg-header z-10 p-4 font-sans text-2xl">
-      <nav className="flex px-4 py-2 lg:container lg:mx-auto text-white text-center items-center justify-between">
+    <section className="sticky top-0 bg-header z-10 p-4 font-sans">
+      <nav className="flex py-2 lg:container lg:mx-auto text-white text-center items-center justify-between">
         <Link href="/">
-          <span className="font-bold hover:text-primary hover:cursor-pointer">
+          <span className="font-bold hover:text-primary hover:cursor-pointer text-2xl">
             Sam Silver
           </span>
         </Link>
-        <div className="hidden md:flex gap-4">
+        <div className="hidden md:flex gap-4  text-xl">
           <Link href="/projects">
             <span className="hover:text-primary hover:cursor-pointer">
               Projects
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,19 +1,32 @@
 import Link from "next/link";
 import fs from "fs";
 
+const PostHeader = ({ slug }) => {
+  return (
+    <div
+      key={slug}
+      className="bg-secondary rounded-lg hover:shadow-lg hover:cursor-pointer p-6"
+    >
+      <Link href="/blog/[slug]" as={"/blog/" + slug}>
+        <h2 className="text-xl text-white font-bold">{slug}</h2>
+      </Link>
+    </div>
+  );
+};
+
 const Blog = ({ paths }) => {
   return (
-    <div className="lg:container lg:mx-auto relative">
-      <h1>Blog</h1>
-      {paths.map((path) => {
-        return (
-          <div key={path}>
-            <Link href="/blog/[slug]" as={"/blog/" + path}>
-              <a>{path}</a>
-            </Link>
-          </div>
-        );
-      })}
+    <div className="lg:container lg:mx-auto relative flex">
+      <div className="w-1/4">
+        <h1>Blog</h1>
+      </div>
+      <div className="w-3/4">
+        <div className="grid grid-cols-2 gap-4">
+          {paths.map((path) => {
+            return PostHeader({ slug: path });
+          })}
+        </div>
+      </div>
     </div>
   );
 };
