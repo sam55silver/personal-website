8c30be3e550ac52a49f51238c49cd8a459d6a968
Sam Silver <sam55silver@gmail.com>
2022-07-12 16:45:54 -0300

    Added more color variations

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 const fancyInputs = (isMessage) => {
   let inputClasses =
-    "px-2 py-2 rounded hover:shadow-md focus:outline-none focus:shadow-md";
+    "px-2 py-2 rounded hover:shadow-md focus:outline-none focus:shadow-md bg-very-dark-base";
 
   return isMessage ? (
     <textarea className={inputClasses + " "} />
@@ -12,9 +12,9 @@ const fancyInputs = (isMessage) => {
 function Footer() {
   return (
     <>
-      <div className="flex flex-col justify-center text-center items-center gap-5 my-10">
+      <div className="flex flex-col text-white justify-center text-center items-center gap-5 my-10">
         <span className="text-5xl py-5">👋 Like what you see? lets talk!</span>
-        <div className="grid grid-rows-8 justify-start text-start my-5 gap-4 text-zinc-500">
+        <div className="grid grid-rows-8 justify-start text-start my-5 text-gray-200 gap-4">
           <div className="grid grid-cols-2 gap-5">
             <span>First Name</span>
             <span>Last Name</span>
@@ -28,13 +28,13 @@ function Footer() {
           <span>Message</span>
           {fancyInputs(true)}
           <div className="flex justify-center items-center mt-8">
-            <button className="bg-primary rounded text-white w-36 py-2 px-5 text-lg m-0 active:bg-darker-primary">
+            <button className="bg-primary rounded w-36 py-2 px-5 text-lg m-0 active:bg-darker-primary">
               Submit
             </button>
           </div>
         </div>
       </div>
-      <div className="bg-slate-200 text-center absolute w-full p-2">
+      <div className="bg-darker-base text-center absolute w-full p-2">
         <span>© 2023 by Sam Silver.</span>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "header": "#8bb4f8",
+        "base": "#8bb4f8",
+        "darker-base": "#799ED9",
+        "very-dark-base": "#566F99",
         "primary": "#ed463e",
-        "darker-primary": "#cf3d36",
+        "darker-primary": "#D94038",
         "secondary": "#98d3a8",
+        "darker-secondary": "#8ABF98",
       },
     },
   },
