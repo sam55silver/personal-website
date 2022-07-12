be701f1098334f22bc3aff36706b760a17e688b9
Sam Silver <sam55silver@gmail.com>
2022-07-12 17:03:45 -0300

    Added more style to footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,19 +1,33 @@
-const fancyInputs = (isMessage) => {
-  let inputClasses =
-    "px-2 py-2 rounded hover:shadow-md focus:outline-none focus:shadow-md bg-very-dark-base";
-
-  return isMessage ? (
-    <textarea className={inputClasses + " "} />
-  ) : (
-    <input className={inputClasses} />
-  );
-};
-
 function Footer() {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  const fancyInputs = (isMessage) => {
+    let inputClasses =
+      "px-2 py-2 rounded hover:shadow-md focus:outline-none focus:shadow-md bg-very-dark-base";
+
+    return isMessage ? (
+      <textarea className={inputClasses + " "} />
+    ) : (
+      <input className={inputClasses} />
+    );
+  };
+
   return (
-    <>
-      <div className="flex flex-col text-white justify-center text-center items-center gap-5 my-10">
-        <span className="text-5xl py-5">👋 Like what you see? lets talk!</span>
+    <footer className="text-white my-10">
+      <div className="flex flex-col justify-center text-center items-center gap-5 my-10 mt-20">
+        <hr className="w-3/4 border-solid border-gray-200 border-2 rounded" />
+        <button
+          onClick={scrollToTop}
+          className="text-5xl hover:cursor-pointer mb-36 mt-10"
+        >
+          ☝️
+        </button>
+        <p className="text-5xl pb-5">👋 Like what you see? lets talk!</p>
         <div className="grid grid-rows-8 justify-start text-start my-5 text-gray-200 gap-4">
           <div className="grid grid-cols-2 gap-5">
             <span>First Name</span>
@@ -37,7 +51,7 @@ function Footer() {
       <div className="bg-darker-base text-center absolute w-full p-2">
         <span>© 2023 by Sam Silver.</span>
       </div>
-    </>
+    </footer>
   );
 }
 
